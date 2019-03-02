26d7847a0a9456a643fe0da8098f3920e5342e6e
gzy <283211498@qq.com>
2019-03-02 22:36:49 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
+
 
 Vue.component('b-button', Button)
 Vue.component('b-icon', Icon)
+Vue.component('b-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="b-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .b-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+
+    > .b-button {
+      border-radius: 0;
+      margin-left: -1px;
+
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
